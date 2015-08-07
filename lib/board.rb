937049ea43b07e2e23d6af96d78a166d92e0f1cf
Holyoke <kid.idioteque@gmail.com>
2015-08-06 21:24:35 -0700

    Setup Board, #display, #fill_column, #init

---
+++ lib/board.rb
@@ -0,0 +1,38 @@
+require "byebug"
+
+class Board
+  attr_reader :board
+  def initialize(cols = 7, rows = 6)
+    @cols, @rows = cols, rows
+    @board = create_board
+  end
+
+  def create_board
+    board = []
+    @cols.times do
+      board << Array.new(@rows)
+    end
+
+    board
+  end
+
+  def fill_column(col, piece)
+    raise "Invalid move, pick a column between 0 and #{7 - 1}" if col > 6
+
+    remaining = @board[col].count { |el| el.nil? }
+    raise "Invalid move, column #{col} is full" if remaining == 0
+    blank_idx = @rows - remaining
+    @board[col][blank_idx] = piece
+
+    puts
+    puts display
+  end
+
+  def display
+    @board.map do |row|
+      row.map do |piece|
+        piece.nil? ? '[ ]' : "[#{piece}]"
+      end.join
+    end.join("\n")
+  end
+end

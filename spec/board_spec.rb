078302f3c0df6e8ecaf3460adee4d6f41975cdc5
Holyoke <kid.idioteque@gmail.com>
2015-08-06 20:46:56 -0700

    Setup RSpec and Board

---
+++ spec/board_spec.rb
@@ -0,0 +1,10 @@
+require "board.rb"
+
+describe "Board" do
+  context "Winning Conditions" do
+    it "checks vertical rows"
+    it "checks horizontal rows"
+    it "checks diagonal NE rows"
+    it "checks diagonal SW rows"
+  end
+end
